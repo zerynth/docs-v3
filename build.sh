5f04e87a9d3ad7f379ea14b248f4b52733691837
Zerynth <dev@zerynth.com>
2021-04-09 16:21:04 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ if [ -z "$DOCPATH" ]; then
     DOCPATH=${PWD}
 fi
 
-docker run --rm -it -p 8081:8000 -v ${PWD}:/docs squidfunk/mkdocs-material:6.1.7
+docker run --rm -it -p 8081:8000 -v ${PWD}:/docs squidfunk/mkdocs-material:6.1.7 $2
