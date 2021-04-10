a190b6709cb5fd883f4673b3ef8fe9b7c208500d
g.baldi <g.baldi@zerynth.com>
2021-04-10 10:45:12 +0200

    fixed CI

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ if [ -z "$DOCPATH" ]; then
     DOCPATH=${PWD}
 fi
 
-docker run --rm -t -p 8081:8000 -v ${PWD}:/docs squidfunk/mkdocs-material:7.1.0 $2
+docker run --rm -t -p 8081:8000 -v ${DOCPATH}:/docs squidfunk/mkdocs-material:7.1.0 $2
